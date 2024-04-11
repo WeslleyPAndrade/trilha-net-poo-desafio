1b0829533c00db8abd16d08773d4111ba68d18de
Weslley Andrade <weslleyp.andrade@gmail.com>
2024-04-11 22:36:26 +0100

    Implementação das classes Nokia e Iphone, inclusão de propriedades na classe Smartphone, testes de execução do projeto.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone,
+Console.WriteLine("Smartphone Nokia: ");
+Nokia smartphoneNokia = new Nokia("123456798", "Modelo 1", "1111111111", 64);
+smartphoneNokia.Ligar();
+smartphoneNokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("Smartphone IPhone: ");
+Iphone smartphoneIphone = new Iphone("987654321", "Modelo 2", "222222222", 64);
+smartphoneIphone.ReceberLigacao();
+smartphoneIphone.InstalarAplicativo("Telegram");
